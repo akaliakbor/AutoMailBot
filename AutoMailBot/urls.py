b7fe9e9873cfcc8ae9665b1bd724f5ec1c6ce8a5
mahfuz1703 <sayhello.mahfuz@gmail.com>
2025-04-20 00:42:29 +0600

    ready for deploy in vercel

--- AutoMailBot/urls.py
+++ AutoMailBot/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
